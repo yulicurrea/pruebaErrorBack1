48b1b9dd9194884f28ebed887048c2137f3887dc
Chiquillo <62142719+Laura-chiquillo@users.noreply.github.com>
2024-09-20 23:57:01 -0500

    Update settings.py
    ok

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ')m(3-ur#5ar4fnyfmsis*9$_3s@m+uz(-r$u)6lx6a*^ln_8j)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
