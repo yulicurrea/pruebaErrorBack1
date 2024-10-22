06e76405fae3810b66a4ee38bfae5c04821cfaf0
Yuli Currea <ycurrea@unbosque.edu.co>
2024-10-21 19:27:18 -0500

    cambios de url

--- app/settings.py
+++ app/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # settings.py
 
 CORS_ALLOWED_ORIGINS = [
-    'https://prueba-error.vercel.app',  # Agrega aquí el origen de tu frontend Angular
+    'https://prueba-error-back1-m9y5.vercel.app/',  # Agrega aquí el origen de tu frontend Angular
     # Otros orígenes permitidos si es necesario...
 ]
 
@@ -157,7 +157,7 @@ CORS_ORIGIN_ALLOW_ALL: True
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:4200',
-    'https://prueba-error.vercel.app'
+    'https://prueba-error-back1-m9y5.vercel.app/'
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
